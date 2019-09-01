d2d201cb6b7ee1647510b257d42163c9e8118cc8
tony <957796031@qq.com>
2019-09-01 15:17:40 +0800

    new wddy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div class="app-container">
+    <router-view></router-view>
+ </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+   .app-container{
+     padding-top:40px;
+     padding-bottom:50px;
+     overflow-x:hidden;
+   }
+
+ .mui-bar-tab .mui-tab-item-tao.mui-active {
+    color: #007aff;
+ }
+.mui-bar-tab .mui-tab-item-tao {
+    display: table-cell;
+    overflow: hidden;
+    width: 1%;
+    height: 50px;
+    text-align: center;
+    vertical-align: middle;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    color: #929292;
+}
+.mui-bar-tab .mui-tab-item-tao .mui-icon {
+    top: 3px;
+    width: 24px;
+    height: 24px;
+    padding-top: 0;
+    padding-bottom: 0;
+}
+
+.mui-bar-tab .mui-tab-item-tao .mui-icon~.mui-tab-label {
+   font-size:11px;
+   display:block;
+   overflow:hidden;
+   text-overflow:ellipsis;
+
 }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="login">
+    <mt-header fixed class="header">
+      <router-link to="javascript:;" slot="left" class="iconfont icon-zuo"></router-link>
+      <router-link to="javascript:;" slot="left">登录注册</router-link>
+    </mt-header>
+    <p>请先进行登录，未注册的手机号会自动为您注册</p>
+    <div class="phone-btn">
+      <mt-field placeholder="输入手机号" v-model="uphone" class="phone"></mt-field>
+      <mt-button v-model="m" disabled>获取验证码</mt-button>
+    </div>
+    <mt-field placeholder="输入验证码"></mt-field>
+    <mt-button disabled class="login-code">登录验证</mt-button>
+    <div>
+      <mt-checklist></mt-checklist>
+      <router-link to>《万达电影用户协议》</router-link>
+    </div>
+    <div class="login-icon">
+        <div><img src="">微博登录</div>
+        <div><img src="">QQ登录</div>
+        <div><img src="">微信登录</div> 
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      value: "1"
+    };
+  }
+};
+</script>
+<style scoped>
+.login {
+  align-items: center;
+  text-align: center;
+  background-color: #fff;
+}
+.header {
+  color: #000;
+  background-color: #fff;
+  border-bottom: 1px solid #aaa;
+}
+.icon-zuo {
+  padding-right: 15px;
+}
+.login p {
+  padding: 15px 0 5px;
+}
+.phone-btn {
+  display: flex;
+}
+.phone {
+  width: 73.5%;
+}
+.login button {
+  font-size: 0.8rem;
+  color: #fff;
+  background-color: #d67e36;
+}
+.login-code {
+  width: 95%;
+}
+.mint-field-core {
+  background-color: #aaa;
+}
+.phone-btn .mint-cell-value .mint-field-core {
+  background-color: gray !important;
+}
+.login-icon{
+    display: flex;
+    justify-content: space-between;
+    padding: 0 20px;
+    align-items: flex-end;
+}
+</style>
+<style>
+.mint-cell-value .mint-field-core {
+  background-color: #e6e6e6;
+}
+</style>
